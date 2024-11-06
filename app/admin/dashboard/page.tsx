dae808d66143c97f84add7c12ecbaf962a53946d
YMarroquin.multilab <yurimarroquin.multilab@gmail.com>
2024-11-06 11:03:50 -0500

    corregir Type "Props" is not valid.

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -1,25 +1,22 @@
-// import PrivateGuard from '@/guards/PrivateGuard'
 import { Breadcrumbs } from '../components/Breadcrumbs'
 import SidebarWrap from '../components/SidebarWrap'
 import '../globals.css'
+
 export const metadata = {
   title: 'Dashboard - TRUELOVE',
   description: 'View to authenticate in the application'
 }
 
-interface Props {
-  children: React.ReactNode
-}
-
-const Layout: React.FC<Props> = ({ children }) => {
+// Define the layout component directly
+const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
-      <div className="min-h-screen w-full bg-[#f5f5f5]">
-        <main className="lg:ml-64 px-2 md:px-4 py-2 min-h-screen bg-transparent">
-          <SidebarWrap />
-          <Breadcrumbs />
-          {children}
-        </main>
-      </div>
+    <div className="min-h-screen w-full bg-[#f5f5f5]">
+      <main className="lg:ml-64 px-2 md:px-4 py-2 min-h-screen bg-transparent">
+        <SidebarWrap />
+        <Breadcrumbs />
+        {children}
+      </main>
+    </div>
   )
 }
 
