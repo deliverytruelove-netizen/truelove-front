f952416f12b9166c2af8206ff5a0c772f5294a60
YMarroquin.multilab <yurimarroquin.multilab@gmail.com>
2024-11-06 09:44:13 -0500

    corregir eslint componente inputr y get.state

--- components/ui/DataTable/utils/get-state-table.util.ts
+++ components/ui/DataTable/utils/get-state-table.util.ts
@@ -5,19 +5,19 @@ import { type Table } from '@tanstack/react-table'
  * @param table Table
  * @returns Object with total rows, first index and last index
  */
-export const getStateTable = (
-  table: Table<any>
+export const getStateTable = <T>(
+  table: Table<T>
 ): {
   totalRows: number
   firstIndex: number
   lastIndex: number
 } => {
   const totalRows = table.getFilteredRowModel().rows.length
-  const pageZize = table.getState().pagination.pageSize
+  const pageSize = table.getState().pagination.pageSize
   const pageIndex = table.getState().pagination.pageIndex
   const rowsPerPage = table.getRowModel().rows.length
-  const firstIndex = pageIndex * pageZize + 1
-  const lastIndex = pageIndex * pageZize + rowsPerPage
+  const firstIndex = pageIndex * pageSize + 1
+  const lastIndex = pageIndex * pageSize + rowsPerPage
 
   return {
     totalRows,
