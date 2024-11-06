386063d174957c9fa04ff5e6f161385e7a34dcf0
YMarroquin.multilab <yurimarroquin.multilab@gmail.com>
2024-11-06 11:13:40 -0500

    corregir template

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -1,23 +1,16 @@
-import { Breadcrumbs } from '../components/Breadcrumbs'
-import SidebarWrap from '../components/SidebarWrap'
-import '../globals.css'
+// src/pages/Usuarios.tsx
 
-export const metadata = {
-  title: 'Dashboard - TRUELOVE',
-  description: 'View to authenticate in the application'
-}
+'use client';
 
-// No need for explicit Props type, children is inferred by Next.js
-const Layout = ({ children }: { children: React.ReactNode }) => {
+import React from 'react';
+import MainLayout from '../components/MainLayout';
+
+const Dashboard: React.FC = () => {
   return (
-    <div className="min-h-screen w-full bg-[#f5f5f5]">
-      <main className="lg:ml-64 px-2 md:px-4 py-2 min-h-screen bg-transparent">
-        <SidebarWrap />
-        <Breadcrumbs />
-        {children}
-      </main>
-    </div>
-  )
-}
+    <MainLayout>
+      <div></div>
+    </MainLayout>
+  );
+};
 
-export default Layout
+export default Dashboard;
