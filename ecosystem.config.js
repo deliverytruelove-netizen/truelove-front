fdb9ae93d564795cbb0f0649d234df0348cff175
Yuri-cmd <yurim011218@gmail.com>
2025-12-10 16:28:36 -0500

    fix: update start script in package.json and add ecosystem.config.js for process management

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps: [
+    {
+      name: "truelove-front",
+      script: "npm",
+      args: "start",
+      env: {
+        NODE_ENV: "production"
+      }
+    }
+  ]
+};
